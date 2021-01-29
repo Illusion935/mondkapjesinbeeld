e8c6ade29a7622d53af5ed47214813bb56734b98
Tom <tomrai@live.nl>
2021-01-29 16:33:23 +0100

    made model work and cleanup

--- functionality/functions.py
+++ functionality/functions.py
@@ -2,11 +2,20 @@ import numpy as np
 import cv2
 from PIL import Image, ImageFont, ImageDraw
 import emoji
+import os
+import tensorflow as tf
+from tensorflow import keras
 
 from .Face import Face
 
 face_cascade = cv2.CascadeClassifier("data\\xml\\haarcascade_frontalface_default.xml")
 mouth_cascade = cv2.CascadeClassifier("data\\xml\\haarcascade_mcs_mouth.xml")
+
+new_model = tf.keras.models.load_model("data/models/Aman_kumar_model.h5")
+cvNet = cv2.dnn.readNetFromCaffe(
+    "data/models/caffemodel/architecture.txt",
+    "data/models/caffemodel/weights.caffemodel",
+)
 
 # Adjust threshold value in range 80 to 105 based on your light.
 bw_threshold = 80
@@ -46,6 +55,48 @@ def draw_on_frame(frame, faces):
     return frame
 
 
+def adjust_gamma(image, gamma=1.0):
+    invGamma = 1.0 / gamma
+    table = np.array([((i / 255.0) ** invGamma) * 255 for i in np.arange(0, 256)])
+    return cv2.LUT(image.astype(np.uint8), table.astype(np.uint8))
+
+
+def caffe_detect_faces(frame, old_faces):
+    gamma = 2.0
+    updated_faces = []
+    img_size = 124
+    h = frame.shape[0]
+    w = frame.shape[1]
+
+    im = adjust_gamma(frame, gamma)
+    im = cv2.resize(im, (300, 300))
+
+    blob = cv2.dnn.blobFromImage(im, 1.0, (300, 300), (104.0, 177.0, 123.0))
+    cvNet.setInput(blob)
+
+    detections = cvNet.forward()
+    for i in range(0, 10):
+        try:
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+            if endX - startX > 0.75 * w or endY - startY > 0.9 * h:
+                break
+            confidence = detections[0, 0, i, 2]
+            if confidence > 0.11:
+                roi = frame[startY:endY, startX:endX]
+                roi = cv2.resize(roi, (img_size, img_size))
+                roi = np.array(roi) / 255.0
+                roi = roi.reshape(1, 124, 124, 3)
+                result = new_model.predict(roi)
+                print(result)
+
+                cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 0, 255), 2)
+        except:
+            pass
+
+    return frame
+
+
 def detect_faces(gray, old_faces, MIN_NEIGHBOURS=5):
     updated_faces = []
     ALLOWED_DIFF = 30
@@ -73,8 +124,17 @@ def detect_faces(gray, old_faces, MIN_NEIGHBOURS=5):
 
 
 def detect_mask(gray, faces):
+    img_size = 124
+
     updated_faces = faces
     for i in range(0, len(faces)):
+        x, y, w, h = faces[i].rio
+        im = gray[y : y + h, x : x + w]
+        im = cv2.resize(im, (img_size, img_size))
+        im = np.array(im) / 255.0
+        im = im.reshape(1, 124, 124, 3)
+        result = new_model.predict(im)
+        print(result)
         updated_faces[i] = detect_mouth(gray, faces[i])
     return updated_faces
 
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from functionality.functions import *
 cap = cv2.VideoCapture(0)
 faces = []
 
+
 if __name__ == "__main__":
     while True:
         ret, frame = cap.read()
@@ -11,9 +12,9 @@ if __name__ == "__main__":
         # Convert image into gray
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        faces = detect_faces(gray, faces)
-        faces = detect_mask(gray, faces)
-        frame = draw_on_frame(frame, faces)
+        frame = caffe_detect_faces(frame, faces)
+        # faces = detect_mask(gray, faces)
+        # frame = draw_on_frame(frame, faces)
 
         cv2.imshow("window", frame)
         k = cv2.waitKey(30) & 0xFF
