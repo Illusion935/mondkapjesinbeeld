b95cdcd0cd8f38aa3c81bf52b0bfa77d7fcae3df
Tom <tomrai@live.nl>
2021-02-11 09:29:11 +0100

    Not asking anymore for user input

--- functionality/interface.py
+++ functionality/interface.py
@@ -7,22 +7,19 @@ def interface():
     root.after(600000, root.destroy)
     root.withdraw()
 
-    with open ("tekst.txt", "r") as file:
-        first_line=file.readline()
+    with open("tekst.txt", "r") as file:
+        first_line = file.readline()
+    gebruiker_input = first_line  # Not asking anymore, just reading it from textfile
+    # gebruiker_input = simpledialog.askstring(title="Mondkapjes in beeld", prompt="Vul hier de boodschap van vandaag in")
 
-    gebruiker_input = simpledialog.askstring(title="Mondkapjes in beeld", prompt="Vul hier de boodschap van vandaag in")
+    # if gebruiker_input == "":
+    #     gebruiker_input=first_line
 
-    if gebruiker_input == "":
-        gebruiker_input=first_line
+    # elif gebruiker_input== None:
+    #     gebruiker_input=first_line
 
-
-    elif gebruiker_input== None:
-        gebruiker_input=first_line
-
-
-    else:
-        with open ("tekst.txt", "w") as file:
-            file.write(gebruiker_input)
-        
+    # else:
+    #     with open ("tekst.txt", "w") as file:
+    #         file.write(gebruiker_input)
 
     return gebruiker_input
